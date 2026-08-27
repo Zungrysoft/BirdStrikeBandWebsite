45c6c9cdc29edd2b63d1c09e8920d5a9f7633c8c
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2026-08-26 23:40:07 -0700

    Revamped gallery page

---
+++ src/helpers/galleryRandomize.js
@@ -0,0 +1,643 @@
+const GALLERY_IMAGES_RAW = [
+    {
+        "filename": "slide_garage_1.jpg",
+        "size": [4044, 3464],
+        "show": "war_machine",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_garage_tom.jpg",
+        "size": [2610, 3702],
+        "title": "Tom in the practice garage",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_garage_alan.jpg",
+        "size": [3484, 4284],
+        "title": "Alan in the practice garage",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_garage_calvin.jpg",
+        "size": [5395, 4284],
+        "title": "Calvin in the practice garage",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_garage_kern.jpg",
+        "size": [4296, 4284],
+        "title": "Kern in the practice garage",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_garage_alex.jpg",
+        "size": [3024, 3894],
+        "title": "Alex in the practice garage",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_garage_alex_2.jpg",
+        "size": [5424, 4046],
+        "title": "Alex in the practice garage",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_garage_gabe.jpg",
+        "size": [2880, 4132],
+        "title": "Gabe in the practice garage",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_garage_gabe_2.jpg",
+        "size": [2520, 3510],
+        "title": "Gabe in the practice garage",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_studio_alex.jpg",
+        "size": [2646, 3666],
+        "title": "Alex recording vocals for Sludge Supper",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_i_drink_your_milkshake.jpg",
+        "size": [2232, 3550],
+        "title": "DRAINAGE ALAN, YOU BOY!",
+        "tags": [],
+        "weight": 5,
+    },
+    {
+        "filename": "slide_gilman_1.jpg",
+        "size": [609, 614],
+        "show": "gilman_open_mic",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_gilman_alex.jpg",
+        "size": [754, 982],
+        "show": "gilman_open_mic" ,
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_tom.jpg",
+        "size": [3803, 2771],
+        "show": "war_machine",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_calvin.jpg",
+        "size": [4031, 3023],
+        "show": "war_machine",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_alex.jpg",
+        "size": [2695, 2410],
+        "show": "war_machine",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_group.jpg",
+        "size": [3828, 3024],
+        "show": "war_machine",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_skate_1.jpg",
+        "size": [3188, 2368],
+        "show": "skate_park",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_skate_2.jpg",
+        "size": [1258, 1376],
+        "show": "skate_park",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_caravan_1.jpg",
+        "size": [942, 605],
+        "show": "caravan",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_caravan_2.jpg",
+        "size": [2896, 2532],
+        "show": "caravan",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_botb_1.jpg",
+        "size": [1159, 1080],
+        "show": "botb",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_botb_2.jpg",
+        "size": [3162, 3526],
+        "show": "botb",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_botb_3.jpg",
+        "size": [2436, 3214],
+        "show": "botb",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_botb_alan.jpg",
+        "size": [689, 1017],
+        "show": "botb",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_botb_calvin.jpg",
+        "size": [738, 907],
+        "show": "botb",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_botb_tom.jpg",
+        "size": [660, 769],
+        "show": "botb",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_blood_on_the_bass.jpg",
+        "size": [3000, 4000],
+        "show": "botb",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_jury_room_1.jpg",
+        "size": [776, 1040],
+        "show": "jury_room",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_2_1.jpg",
+        "size": [1964, 1223],
+        "show": "war_machine_2",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_2_2.jpg",
+        "size": [1463, 1488],
+        "show": "war_machine_2",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_2_alan.jpg",
+        "size": [1406, 1496],
+        "show": "war_machine_2",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_2_alex.jpg",
+        "size": [1424, 1832],
+        "show": "war_machine_2",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_2_hannan.jpg",
+        "size": [1463, 2048],
+        "show": "war_machine_2",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_2_mosh_pit.jpg",
+        "size": [1463, 1901],
+        "show": "war_machine_2",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_2_worship.jpg",
+        "size": [1883, 1280],
+        "show": "war_machine_2",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_war_machine_2_sermon.jpg",
+        "size": [1256, 690],
+        "show": "war_machine_2",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_yard_1.jpg",
+        "size": [1114, 910],
+        "show": "the_yard",
+        "tags": [],
+        "weight": 100,
+    },
+    /*{
+        "filename": "slide_caravan_3_1.jpg",
+        "size": [3516, 2880],
+        "show": "caravan_3",
+        "tags": [],
+        "weight": 100,
+    },*/
+    {
+        "filename": "slide_caravan_3_2.jpg",
+        "size": [4104, 2880],
+        "show": "caravan_3",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_caravan_3_alan.jpg",
+        "size": [2442, 2880],
+        "show": "caravan_3",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_caravan_3_calvin.jpg",
+        "size": [1720, 2880],
+        "show": "caravan_3",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_caravan_3_kern.jpg",
+        "size": [1716, 2880],
+        "show": "caravan_3",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel_1.jpg",
+        "size": [1892, 2356],
+        "show": "the_vessel",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel_2.jpg",
+        "size": [1885, 1980],
+        "show": "the_vessel",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel_3.jpg",
+        "size": [1207, 1616],
+        "show": "the_vessel",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel_4.jpg",
+        "size": [1586, 2188],
+        "show": "the_vessel",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel2_1.jpg",
+        "size": [3208, 5027],
+        "show": "vessel_festival",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel2_2.jpg",
+        "size": [2552, 3504],
+        "show": "vessel_festival",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel2_3.jpg",
+        "size": [2274, 3724],
+        "show": "vessel_festival",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel2_4.jpg",
+        "size": [2430, 3832],
+        "show": "vessel_festival",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel2_5.jpg",
+        "size": [3640, 6000],
+        "show": "vessel_festival",
+        "tags": [],
+        "weight": 100,
+    },
+    {
+        "filename": "slide_vessel2_6.jpg",
+        "size": [3510, 3706],
+        "show": "vessel_festival",
+        "tags": [],
+        "weight": 100,
+    },
+];
+
+const MAX_WIDTH = 700;
+const MAX_HEIGHT = 700;
+const GALLERY_IMAGES = GALLERY_IMAGES_RAW.map((img) => {
+    let sizeCompressed = img.size;
+
+    const width = sizeCompressed[0];
+    const height = sizeCompressed[1];
+    if (width > MAX_WIDTH || height > MAX_HEIGHT) {
+        const scale = Math.min(MAX_WIDTH / width, MAX_HEIGHT / height);
+        sizeCompressed = [Math.floor(width * scale), Math.floor(height * scale)];
+    }
+    
+    return {
+        ...img,
+        sizeCompressed: sizeCompressed,
+    };
+})
+
+export const GALLERY_SPACING_PX = 16;
+
+const IMAGE_CROP_COST = 1;
+const IMAGE_CROP_COST_GROWTH = 1.1;
+const GAP_ALIGNMENT_COST = 60;
+const GAP_ALIGNMENT_COST_DIST = 64;
+const GALLERY_SETS = 20;
+const GALLERY_SET_ARRANGEMENTS = 120;
+const ADD_IMAGE_COST = 200;
+const EMPTY_COLUMN_COST = 1_000_000_000;
+const IMAGE_COUNT_BY_NUM_COLUMNS = [8, 11, 16, 19, 23, 25];
+
+
+/*
+Pick 17 images to start with. 
+Choose random combinations, cropping each image by a random amount to get them to line up
+Check badness
+*/
+
+
+export function pickGalleryImages(numColumns, columnWidthPx) {
+    columnWidthPx = Math.floor(columnWidthPx);
+    const imageCount = IMAGE_COUNT_BY_NUM_COLUMNS[Math.min(numColumns + 1, IMAGE_COUNT_BY_NUM_COLUMNS.length - 1)];
+
+    let possibilities = [];
+
+    // Pick N different gallery image sets
+    for (let i = 0; i < GALLERY_SETS; i ++) {
+        const images = pickRandomImages(imageCount);
+
+        // Pick N different random arrangements of this image set
+        for (let j = 0; j < GALLERY_SET_ARRANGEMENTS; j ++) {
+
+            // Init columns array
+            let columns = [];
+            for (let k = 0; k < numColumns; k ++) {
+                columns.push([]);
+            }
+
+            for (const image of images) {
+                columns[Math.floor(Math.random() * numColumns)].push(image);
+            }
+
+            columns = cropColumns(columns, columnWidthPx);
+
+            possibilities.push(columns);
+        }
+    }
+
+    // Evalute the randomly generated possibilities for lowest badness
+    let lowestBadness = 9999999999999;
+    let highestBadness = 0;
+    let lowestBadnessColumns = null;
+    for (const possibility of possibilities) {
+        const possibilityBadness = getBadness(possibility);
+        if (possibilityBadness < lowestBadness) {
+            lowestBadness = possibilityBadness;
+            lowestBadnessColumns = possibility;
+        }
+        if (possibilityBadness > highestBadness) {
+            highestBadness = possibilityBadness;
+        }
+    }
+
+    console.log("BADNESS: ", lowestBadness, highestBadness)
+    
+    return lowestBadnessColumns;
+}
+
+function getImageNaturalHeight(img, columnWidthPx) {
+    return columnWidthPx * (img.size[1] / img.size[0]);
+}
+
+function getColumnHeight(column, columnWidthPx) {
+    let height = Math.max(column.length - 1, 0) * GALLERY_SPACING_PX;
+    for (const img of column) {
+        height += getImageNaturalHeight(img, columnWidthPx);
+    }
+    return height;
+}
+
+function allocateIntegerHeights(naturalHeights, targetSum) {
+    const n = naturalHeights.length;
+    if (n === 0) {
+        return [];
+    }
+
+    targetSum = Math.max(Math.round(targetSum), n);
+
+    const locked = new Array(n).fill(false);
+    const desired = new Array(n).fill(1);
+
+    for (let iter = 0; iter < n; iter++) {
+        let lockedCount = 0;
+        const unlocked = [];
+        for (let i = 0; i < n; i++) {
+            if (locked[i]) {
+                lockedCount += 1;
+            } else {
+                unlocked.push(i);
+            }
+        }
+
+        const remaining = targetSum - lockedCount;
+        const unlockedNatural = unlocked.reduce((sum, i) => sum + naturalHeights[i], 0);
+        const cropEach = unlocked.length > 0 ? (unlockedNatural - remaining) / unlocked.length : 0;
+
+        let lockedNew = false;
+        for (const i of unlocked) {
+            const h = naturalHeights[i] - cropEach;
+            if (h < 1) {
+                locked[i] = true;
+                desired[i] = 1;
+                lockedNew = true;
+            } else {
+                desired[i] = h;
+            }
+        }
+        if (!lockedNew) {
+            break;
+        }
+    }
+
+    const heights = desired.map((h) => Math.max(Math.floor(h), 1));
+    let remainder = targetSum - heights.reduce((sum, h) => sum + h, 0);
+
+    const order = desired
+        .map((h, i) => ({ i, frac: h - Math.floor(h) }))
+        .sort((a, b) => b.frac - a.frac);
+
+    if (remainder > 0) {
+        for (let k = 0; k < remainder; k++) {
+            heights[order[k % n].i] += 1;
+        }
+    } else if (remainder < 0) {
+        const byHeight = heights
+            .map((h, i) => ({ i, h }))
+            .sort((a, b) => b.h - a.h);
+        let left = -remainder;
+        for (let k = 0; left > 0 && k < n * 8; k++) {
+            const i = byHeight[k % n].i;
+            if (heights[i] > 1) {
+                heights[i] -= 1;
+                left -= 1;
+            }
+        }
+    }
+
+    return heights;
+}
+
+function cropColumns(columns, columnWidthPx) {
+    const columnsRet = columns.map((column) => column.map((img) => ({ ...img })));
+
+    let minHeight = Infinity;
+    for (const column of columnsRet) {
+        if (column.length === 0) {
+            continue;
+        }
+        minHeight = Math.min(minHeight, getColumnHeight(column, columnWidthPx));
+    }
+
+    if (!Number.isFinite(minHeight)) {
+        return columnsRet;
+    }
+
+    const targetHeight = Math.floor(minHeight);
+
+    for (const column of columnsRet) {
+        if (column.length === 0) {
+            continue;
+        }
+
+        const gap = Math.max(column.length - 1, 0) * GALLERY_SPACING_PX;
+        const targetContentHeight = targetHeight - gap;
+        const naturalHeights = column.map((img) => getImageNaturalHeight(img, columnWidthPx));
+        const displayHeights = allocateIntegerHeights(naturalHeights, targetContentHeight);
+
+        for (let i = 0; i < column.length; i++) {
+            column[i].displayHeight = displayHeights[i];
+            column[i].crop = Math.max(0, naturalHeights[i] - displayHeights[i]);
+        }
+    }
+
+    return columnsRet;
+}
+
+function arrangeImagesIntoColumns(images, numColumns) {
+    const columns = [];
+    for (let k = 0; k < numColumns; k++) {
+        columns.push([]);
+    }
+
+    for (const image of images) {
+        columns[Math.floor(Math.random() * numColumns)].push(image);
+    }
+
+    return columns;
+}
+
+function getBadness(columns) {
+    // Punish having to crop images to line up columns
+    let badnessFromCrop = 0;
+    for (const column of columns) {
+        for (const img of column) {
+            badnessFromCrop += (img.crop * IMAGE_CROP_COST) ** IMAGE_CROP_COST_GROWTH;
+        }
+    }
+
+    let emptyColumnCost = 0;
+    for (const column of columns) {
+        if (column.length === 0) {
+            emptyColumnCost += EMPTY_COLUMN_COST;
+        }
+    }
+
+    return badnessFromCrop + emptyColumnCost;
+}
+
+function weightedPick(elements) {
+    let totalWeight = 0;
+    for (const el in elements) {
+        totalWeight += el.weight ?? 0;
+    }
+    let pickedWeight = Math.random() * totalWeight;
+    for (const el in elements) {
+        pickedWeight -= el.weight ?? 0;
+        if (pickedWeight < 0) {
+            return el;
+        }
+    }
+    return elements[Math.floor(Math.random() * elements.length)];
+}
+
+function pickRandomImages(count) {
+    let imageSet = new Set();
+    while (imageSet.size < count) {
+        const pickedImage = weightedPick(GALLERY_IMAGES);
+        imageSet.add(pickedImage);
+    }
+    return imageSet;
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,69 +1,10 @@
-import { Box, ImageList, ImageListItem, useMediaQuery, useTheme } from "@mui/material";
-import { useLayoutEffect, useState } from "react";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { useLayoutEffect, useRef, useState } from "react";
 import shows from '../data/shows.json';
 import { humanizeDate } from "../components/UpcomingShows";
+import { GALLERY_SPACING_PX, pickGalleryImages } from "../helpers/galleryRandomize";
 
 const images = require.context('../../public/images/compressed', true);
-
-const GALLERY_IMAGES = [
-    { "filename": "slide_garage_1.jpg", "size": [4044, 3464], "show": "war_machine" },
-    { "filename": "slide_garage_tom.jpg", "size": [2610, 3702], "title": "Tom in the practice garage" },
-    { "filename": "slide_garage_alan.jpg", "size": [3484, 4284], "title": "Alan in the practice garage" },
-    { "filename": "slide_garage_calvin.jpg", "size": [5395, 4284], "title": "Calvin in the practice garage" },
-    { "filename": "slide_garage_kern.jpg", "size": [4296, 4284], "title": "Kern in the practice garage" },
-    { "filename": "slide_garage_alex.jpg", "size": [3024, 3894], "title": "Alex in the practice garage" },
-    { "filename": "slide_garage_alex_2.jpg", "size": [5424, 4046], "title": "Alex in the practice garage" },
-    { "filename": "slide_garage_gabe.jpg", "size": [2880, 4132], "title": "Gabe in the practice garage" },
-    { "filename": "slide_garage_gabe_2.jpg", "size": [2520, 3510], "title": "Gabe in the practice garage" },
-    // { "filename": "slide_garage_hannan.jpg", "size": [2688, 3520], "title": "In the practice garage" },
-    { "filename": "slide_studio_alex.jpg", "size": [2646, 3666], "title": "Alex recording vocals for Sludge Supper" },
-    { "filename": "slide_i_drink_your_milkshake.jpg", "size": [2232, 3550], "title": "DRAINAGE ALAN, YOU BOY!" },
-    { "filename": "slide_gilman_1.jpg", "size": [609, 614], "show": "gilman_open_mic" },
-    { "filename": "slide_gilman_alex.jpg", "size": [754, 982], "show": "gilman_open_mic"  },
-    { "filename": "slide_war_machine_tom.jpg", "size": [3803, 2771], "show": "war_machine" },
-    { "filename": "slide_war_machine_calvin.jpg", "size": [4031, 3023], "show": "war_machine" },
-    { "filename": "slide_war_machine_alex.jpg", "size": [2695, 2410], "show": "war_machine" },
-    { "filename": "slide_war_machine_group.jpg", "size": [3828, 3024], "show": "war_machine" },
-    { "filename": "slide_skate_1.jpg", "size": [3188, 2368], "show": "skate_park" },
-    { "filename": "slide_skate_2.jpg", "size": [1258, 1376], "show": "skate_park" },
-    { "filename": "slide_caravan_1.jpg", "size": [942, 605], "show": "caravan" },
-    { "filename": "slide_caravan_2.jpg", "size": [2896, 2532], "show": "caravan" },
-    { "filename": "slide_botb_1.jpg", "size": [1159, 1080], "show": "botb" },
-    { "filename": "slide_botb_2.jpg", "size": [3162, 3526], "show": "botb" },
-    { "filename": "slide_botb_3.jpg", "size": [2436, 3214], "show": "botb" },
-    { "filename": "slide_botb_alan.jpg", "size": [689, 1017], "show": "botb" },
-    { "filename": "slide_botb_calvin.jpg", "size": [738, 907], "show": "botb" },
-    { "filename": "slide_botb_tom.jpg", "size": [660, 769], "show": "botb" },
-    { "filename": "slide_blood_on_the_bass.jpg", "size": [3000, 4000], "show": "botb" },
-    { "filename": "slide_jury_room_1.jpg", "size": [776, 1040], "show": "jury_room" },
-    { "filename": "slide_war_machine_2_1.jpg", "size": [1964, 1223], "show": "war_machine_2" },
-    { "filename": "slide_war_machine_2_2.jpg", "size": [1463, 1488], "show": "war_machine_2" },
-    { "filename": "slide_war_machine_2_alan.jpg", "size": [1406, 1496], "show": "war_machine_2" },
-    { "filename": "slide_war_machine_2_alex.jpg", "size": [1424, 1832], "show": "war_machine_2" },
-    { "filename": "slide_war_machine_2_hannan.jpg", "size": [1463, 2048], "show": "war_machine_2" },
-    { "filename": "slide_war_machine_2_mosh_pit.jpg", "size": [1463, 1901], "show": "war_machine_2" },
-    { "filename": "slide_war_machine_2_worship.jpg", "size": [1883, 1280], "show": "war_machine_2" },
-    { "filename": "slide_war_machine_2_sermon.jpg", "size": [1256, 690], "show": "war_machine_2" },
-    { "filename": "slide_yard_1.jpg", "size": [1114, 910], "show": "the_yard" },
-    // { "filename": "slide_caravan_3_1.jpg", "size": [3516, 2880], "show": "caravan_3" },
-    { "filename": "slide_caravan_3_2.jpg", "size": [4104, 2880], "show": "caravan_3" },
-    { "filename": "slide_caravan_3_alan.jpg", "size": [2442, 2880], "show": "caravan_3" },
-    { "filename": "slide_caravan_3_calvin.jpg", "size": [1720, 2880], "show": "caravan_3" },
-    { "filename": "slide_caravan_3_kern.jpg", "size": [1716, 2880], "show": "caravan_3" },
-    { "filename": "slide_vessel_1.jpg", "size": [1892, 2356], "show": "the_vessel" },
-    { "filename": "slide_vessel_2.jpg", "size": [1885, 1980], "show": "the_vessel" },
-    { "filename": "slide_vessel_3.jpg", "size": [1207, 1616], "show": "the_vessel" },
-    { "filename": "slide_vessel_4.jpg", "size": [1586, 2188], "show": "the_vessel" },
-];
-
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
 
 function getImageTitleFromShow(show) {
     if (!show || !shows[show]) {
@@ -80,45 +21,61 @@ function getImageTitleFromShow(show) {
 function GalleryPage() {
     const theme = useTheme();
     const isCompact = useMediaQuery(theme.breakpoints.down('sm'));
-    const isModeratelyCompact = useMediaQuery(theme.breakpoints.down('md'))
-    const isSomewhatCompact = useMediaQuery(theme.breakpoints.down('xxl'))
-    const [galleryImages, setGalleryImages] = useState([]);
+    const isModeratelyCompact = useMediaQuery(theme.breakpoints.down('md'));
+    const isSomewhatCompact = useMediaQuery(theme.breakpoints.down('xxl'));
+    
+    const containerRef = useRef(null);
+    const [galleryColumns, setGalleryColumns] = useState([]);
+    const [columnWidthPx, setColumnWidthPx] = useState(0);
 
     useLayoutEffect(() => {
-        let shuffledGalleryImages = [...GALLERY_IMAGES];
-        shuffleArray(shuffledGalleryImages);
-        setGalleryImages(shuffledGalleryImages);
-    }, []);
+        const width = containerRef.current.getBoundingClientRect().width;
+        
+        let numCols = 5;
+        if (isCompact) numCols = 2;
+        if (isModeratelyCompact) numCols = 3;
+        if (isSomewhatCompact) numCols = 4;
 
-    let numCols = 5;
-    if (isCompact) {
-        numCols = 2;
-    }
-    else if (isModeratelyCompact) {
-        numCols = 3;
-    }
-    else if (isSomewhatCompact) {
-        numCols = 4;
-    }
+        const columnWidth = Math.floor((width - ((numCols - 1) * GALLERY_SPACING_PX)) / numCols);
+
+        const columns = pickGalleryImages(numCols, columnWidth);
+
+        setColumnWidthPx(columnWidth);
+        setGalleryColumns(columns);
+    }, []);
 
     return(
         <Box sx={{ margin: 'auto', width: 'min(100%, 1600px)', minHeight: isCompact ? null : '101vh' }}>
-            <Box sx={{ marginLeft: '16px', marginRight: '16px', marginBottom: '16px' }}>
-                <ImageList variant="masonry" cols={numCols} gap={16}>
-                    {galleryImages.map((imgData) => (
-                        <ImageListItem key={imgData.filename}>
-                            <img
-                                src={images(`./${imgData.filename}`)}
-                                title={imgData.title ?? getImageTitleFromShow(imgData.show)}
-                                loading="lazy"
+            <Box sx={{ width: 'auto', marginLeft: `${GALLERY_SPACING_PX}px`, marginRight: `${GALLERY_SPACING_PX}px`, marginBottom: `${GALLERY_SPACING_PX}px`, display: 'flex', gap: `${GALLERY_SPACING_PX}px` }} ref={containerRef}>
+                {galleryColumns.map((column, i) => (
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: `${GALLERY_SPACING_PX}px`, width: columnWidthPx, flex: `0 0 ${columnWidthPx}px`, minWidth: 0 }} key={i}>
+                        {column.map((imgData) => (
+                            <div
                                 style={{
-                                    width: '100%',
-                                    aspectRatio: (imgData.size[0] / imgData.size[1]) ?? '3/2',
+                                    overflow: 'hidden',
+                                    height: imgData.displayHeight,
+                                    flexShrink: 0,
+                                    lineHeight: 0,
                                 }}
-                            />
-                        </ImageListItem>
-                    ))}
-                </ImageList>
+                                key={imgData.filename}
+                            >
+                                <img
+                                    src={images(`./${imgData.filename}`)}
+                                    title={imgData.title ?? getImageTitleFromShow(imgData.show)}
+                                    loading="lazy"
+                                    alt={imgData.title ?? getImageTitleFromShow(imgData.show)}
+                                    style={{
+                                        display: 'block',
+                                        width: '100%',
+                                        height: '100%',
+                                        objectFit: 'cover',
+                                        objectPosition: 'center',
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </Box>
+                ))}
             </Box>
             <Box sx={{ height: '1px' }}/>
         </Box>
